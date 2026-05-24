9de93e9a3fb91af035d023586f3a2f52bf379a63
rv6475516-del <rv6475516@gmail.com>
2026-05-24 19:04:52 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,19 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+operation = input("Enter operation (+, -, *, /): ")
+
+if operation == "+":
+    print("Result =", num1 + num2)
+
+elif operation == "-":
+    print("Result =", num1 - num2)
+
+elif operation == "*":
+    print("Result =", num1 * num2)
+
+elif operation == "/":
+    print("Result =", num1 / num2)
+
+else:
+    print("Invalid operation")
